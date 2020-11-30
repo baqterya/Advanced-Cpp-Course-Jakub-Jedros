4bd3ab4c9ef82f79f49ffba0bf9c9b8cd9064421
baqterya <jakubjedros1999@gmail.com>
2020-11-30 14:46:16 +0100

    lista 5

---
+++ lista-5/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <mutex>
+#include <future>
+
+
+int returnThreadId() {
+    static std::mutex m;
+    static int index = 0;
+
+    thread_local int id = [&]{
+        std::lock_guard<std::mutex> lock(m);
+        return index++;
+    }();
+        
+    return id;
+}
+
+void printString(std::string s){
+    static std::mutex m;
+    std::lock_guard<std::mutex> lock(m);
+    std::cout << "\nna wątku " << returnThreadId() << " printuję " << s << std::endl;
+}
+
+void asyncFunction(std::launch policy, int n){
+    if (n > 0) {
+        auto a = std::async(policy, asyncFunction, policy, n - 1);
+        a.get();
+
+        printString("stringa w asyncFunction");
+    }
+}
+
+int main() {
+ 
+    auto a = std::async(std::launch::async, asyncFunction, std::launch::async, 12);
+
+
+    return 0;
+}
