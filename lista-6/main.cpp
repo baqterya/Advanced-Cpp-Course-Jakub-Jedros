8ae91fb3a1b9413915c72693ababdecd9ca87f54
baqterya <jakubjedros1999@gmail.com>
2020-12-06 19:52:00 +0100

    ...

--- lista-6/main.cpp
+++ lista-6/main.cpp
@@ -6,19 +6,19 @@
 #include <numeric>
 
 void iloczynSkal(std::vector<double> &a, std::vector<double> &b, std::promise<double> &p) {
-    double wynik;
-    
+       
     try {
         if (a.size() != b.size()) {
             throw std::runtime_error("vector_size_err");
         }
-        wynik = std::inner_product(a.begin(), a.end(), b.begin(), 0.0);
 
-            p.set_value(wynik);              
+        p.set_value( std::inner_product(a.begin(), a.end(), b.begin(), 0.0) );              
     }
     catch (std::exception &e) {
         p.set_exception(std::current_exception());
-    }        
+    }     
+
+    // return p;   
 }
 
 int main() {
@@ -27,11 +27,13 @@ int main() {
     std::vector<double> b = {1, 2, 3, 4};
     std::vector<double> c = {1, 2, 3, 4};
     std::array<std::thread, 10> threads;
+    double suma = 0;
 
+    
     for (auto &t : threads) {
         std::promise<double> prom;
-        std::future<double> fut = prom.get_future();
-
+        std::future<double> fut = prom.get_future(); // = iloczynSkal(a, b, prom);
+    
         t = std::thread ([&]{
             iloczynSkal(a, b, prom);
             try {
@@ -41,16 +43,18 @@ int main() {
                 std::cout << "exception caught: " << e.what() << '\n';
             }
             
+            
         });
         
         t.join();
     }
-
-    /*std::cout << "waiting...\n";
+/*
+    std::cout << "waiting...\n";
 
     for (auto &t : threads) {
         t.join();
-    }*/
+    }
+  */  
 
     return 0;
 }
